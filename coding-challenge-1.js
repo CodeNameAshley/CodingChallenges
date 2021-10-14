b2df8c6b922303f2ac187475cb15b4c94573f0d2
ashleyn <ashleynavo@googlemail.com>
2021-10-14 22:54:34 +0100

    first coding challenge

---
+++ coding-challenge-1.js
@@ -0,0 +1,12 @@
+let markheight = 1.69;
+let markweight = 78;
+
+let johnheight = 1.95;
+let johnweight = 92;
+
+
+let marksBMI =  (markweight/ (markheight *markheight))
+
+let johnsBMI = (johnweight / (johnheight * johnheight))
+
+const markHigherBMI =  marksBMI < johnsBMI; 

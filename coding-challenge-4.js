577b49371070584c2a4cdd80aa115c611b9b6646
ashleyn <ashleynavo@googlemail.com>
2021-10-16 00:40:43 +0100

    coding challenge #4 w/ ternary operator

--- coding-challenge-4.js
+++ coding-challenge-4.js
@@ -1,3 +1,4 @@
+// long version
 const  tip20 = (20/100);
 
 const  tip15 = (15/100);
@@ -13,8 +14,6 @@ const bill2 = value2 >= 50 && value2 <= 300 ? console.log(`The bill is ${value2}
 const bill3 = value3 >= 50 && value3 <= 300 ? console.log(`The bill is ${value3}, tip should be ${ value3 * tip15},  total bill is ${value3 + (value3 * tip15)}`) : console.log(`The bill is ${value3}, tip should be ${value3 * tip20}, total bill is ${value3 + (value3 * tip20)}`);
 
 // short version 
-
 const bill = 40;
 
 const tip = bill >= 50 && <= 300 ? bill * 0.15 : bill * 0.2;
-
